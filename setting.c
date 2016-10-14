84650412b8e61574ff47c2278725e170665723db
Soufiane Aourinmouche <aourinmo2u@fst-ats-310-l10.fst.site.univ-lorraine.fr>
2016-10-14 09:46:14 +0200

    Mise en place de la map ( 25 % done )

--- setting.c
+++ setting.c
@@ -40,24 +40,59 @@ void set_map()
     {
       MAP[0][i] = 1;
     }
-  // Draw two columns of sand 
+
+
+    // Mes ajouts commencent d'ici  : Mise en place de la MAP " manuellement "
+
+  // Draw two columns of sand              
   for ( i=0 ; i < MAPheight-2 ; i++)
     {
-      MAP[1][i] = 2;
-      MAP[2][i] = 2;
+      MAP[1][i] = MAP[2][i] = 2;
     }
+  for(i = 0 ; i < MAPlength ; i++)
+    {
+      MAP[i][MAPheight-4] = MAP[i][MAPheight-3] = 2;
+    }
+
+
   // Draw three columns of trees
   for( i=0 ; i < 7 ; i++)
     {
-      MAP[3][i] = 3;
-      MAP[4][i] = 3;
-      MAP[5][i] = 3;
+      MAP[3][i] = MAP[4][i] = MAP[5][i] = 3 ;        
     }
-  // Completting drawin trees columns with just two trees each line 
-  for ( i=7 ; i < MAPheight-2 ; i++){
-    MAP[3][i] = 3;
-    MAP[4][i] = 3;
+  for ( j=5 ; j<10 ; j++ )
+    {
+      for(i=0 ; i<5 ; i++)
+	{
+	  MAP[j][i] = 3;
+	}
+    }
+  for ( j = 5 ; j < MAPheight-4 ; j++){
+    MAP[8][j] = MAP[9][j] = 3;
   }
+
+
+
+  // Completting drawing trees columns with just two trees each line 
+  for ( i=7 ; i < MAPheight-4 ; i++){
+    MAP[3][i] = MAP[4][i] = 3;
+  }
+
+
+
+  // Drawing the ground_land 
+  for ( i = 10 ; i < 18 ; i++ )
+    {
+      MAP[i][7] = MAP[i][6] = 4 ;
+    }
+  
+
+  // Drawing the trees 
+  MAP[5][39] = MAP[5][38] = MAP[6][38] = MAP[6][39] = MAP[7][38] = MAP[7][39] = MAP[6][37] = MAP[7][37] = MAP[7][36]  =  MAP[6][7] = MAP[5][7] = 3 ;
+  MAP[8][6] =  MAP[9][6] =   MAP[8][7] =  MAP[9][7] = 0  ;
+
+
+  MAP [9][8] = 0 ; // A supprimer , juste pour laisser passer le personnage afin de parcourir la map 
 }
 
 
