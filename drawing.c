4c909d38304fabd1d4a8a5a887dea622e4f40f44
Alex Ginestra <ginestra3u@fst-ats-312-l13.fst.site.univ-lorraine.fr>
2016-11-03 17:00:43 +0100

    correcting fucking bug

--- drawing.c
+++ drawing.c
@@ -1,16 +1,7 @@
 #include "SDL.h"
+#include "global_variable.c"
+
 #define SPRITE_SIZE    32
-
-/* extern variable */
-extern SDL_Surface *screen, *temp, *sprite, *grass, *water, *tree, *dirt, *sand, *pause;
-extern SDL_Surface *dirtg_d, *dirtg_dl, *dirtg_dr, *dirtg_l, *dirtg_r, *dirtg_u, *dirtg_ul, *dirtg_ur;
-extern SDL_Surface *sandg_dl, *sandg_dr, *sandg_l, *sandg_r, *sandg_top, *sandg_ucl, *sandg_ucr, *sandg_ul, *sandg_ur;
-extern SDL_Surface *sandw_dl, *sandw_dr, *sandw_l, *sandw_r, *sandw_top, *sandw_ucl, *sandw_ucr, *sandw_ul, *sandw_ur;
-extern SDL_Surface *rockwall, *rockwall_dl, *rockwall_dr, *rockwall_l, *rockwall_r, *rockwall_top, *rockwall_ucl, *rockwall_ucr, *rockwall_ul, *rockwall_ur;
-
-extern SDL_Rect rcSprite, rcSrcSprite, rcGrass, rcWater, rcSand, rcTree, rcDirt, rcRock, rcPause;
-extern int coordplayerx, coordplayery;
-
 
 
 
@@ -38,7 +29,7 @@ void draw_rockwallPM(int i, int j);
 
 
 
-SDL_Surface kind_of_dirt(int x, int y);
+SDL_Surface* kind_of_dirt(int x, int y);
 
 //SDL_Surface kinf_of_rockwall(int i, int j);
 
@@ -173,15 +164,15 @@ void draw_dirtPM(int i, int j, int x, int y)
 {
   rcDirt.x = i * SPRITE_SIZE;
   rcDirt.y = j * SPRITE_SIZE;
-  *temp = kind_of_dirt(x,y);
-  SDL_BlitSurface(temp, NULL, screen, &rcDirt);
+  temp = kind_of_dirt(x,y);
+  SDL_BlitSurface(temp,NULL, screen, &rcDirt);
 }
 
 void draw_dirtGM(int i, int j, int x, int y)
 {
   rcDirt.x = i * SPRITE_SIZE - (coordplayerx % SPRITE_SIZE);
   rcDirt.y = j * SPRITE_SIZE - (coordplayery % SPRITE_SIZE);
-  *temp = kind_of_dirt(x,y);
+  temp =  kind_of_dirt(x,y);
   SDL_BlitSurface(temp, NULL, screen, &rcDirt);
 }
 
@@ -376,7 +367,7 @@ void draw_rockwall_urGM(int i, int j)
 
 
 
-SDL_Surface kind_of_dirt(int x, int y)
+SDL_Surface* kind_of_dirt(int x, int y)
 {
  
   //checking if it's the top and then left or right
@@ -386,30 +377,30 @@ SDL_Surface kind_of_dirt(int x, int y)
 	{
 	  if(!MAP[x+1][y] && ! MAP[x][y+1])
 	    {
-	      return *dirtg_dr;
+	      return dirtg_dr;
 	    }
 	  if( !MAP[x][y+1] )
 	    {
-	      return *dirtg_d;
+	      return dirtg_d;
 	    }
 	  if( !MAP[x+1][y] )
 	    {
-	      return *dirtg_r;
+	      return dirtg_r;
 	    }
 	}
       if( x == MAPlength-1 ) //right top corner
 	{
 	  if( !MAP[x-1][y] && !MAP[x][y+1] )
 	    {
-	      return *dirtg_dl;
+	      return dirtg_dl;
 	    } 
 	  if( !MAP[x][y+1] )
 	    {
-	      return *dirtg_d;
+	      return dirtg_d;
 	    }
 	  if( !MAP[x-1][y] )
 	    {
-	      return *dirtg_l;
+	      return dirtg_l;
 	    }
 	}
       else //top
@@ -418,21 +409,21 @@ SDL_Surface kind_of_dirt(int x, int y)
 	    {
 	      if( !MAP[x+1][y] )
 		{
-		  return *dirtg_dr;
+		  return dirtg_dr;
 		}
 	      if( !MAP[x-1][y] )
 		{
-		  return *dirtg_dl;
+		  return dirtg_dl;
 		}
-	      return *dirtg_d;
+	      return dirtg_d;
 	    }
 	  if( !MAP[x+1][y] )
 	    {
-	      return *dirtg_r;
+	      return dirtg_r;
 	    }
 	  if( !MAP[x-1][y] )
 	    {
-	      return *dirtg_l;
+	      return dirtg_l;
 	    }
 	}
     }
@@ -444,30 +435,30 @@ SDL_Surface kind_of_dirt(int x, int y)
 	{
 	  if(!MAP[x+1][y] && !MAP[x][y-1])
 	    {
-	      return *dirtg_ur;
+	      return dirtg_ur;
 	    }
 	  if(!MAP[x][y-1])
 	    {
-	      return *dirtg_u;
+	      return dirtg_u;
 	    }
 	  if(!MAP[x+1][y])
 	    {
-	      return *dirtg_r;
+	      return dirtg_r;
 	    }
 	}
       if( x == MAPlength-1 ) //right down corner
 	{
 	  if( !MAP[x-1][y] && !MAP[x][y-1] )
 	    {
-	      return *dirtg_ul;
+	      return dirtg_ul;
 	    }
 	  if( !MAP[x][y-1] )
 	    {
-	      return *dirtg_u;
+	      return dirtg_u;
 	    }
 	  if( !MAP[x-1][y] )
 	    {
-	      return *dirtg_l;
+	      return dirtg_l;
 	    }
 	}
       else //bottom
@@ -476,21 +467,21 @@ SDL_Surface kind_of_dirt(int x, int y)
 	    {
 	      if( !MAP[x+1][y] )
 		{
-		  return *dirtg_ur;
+		  return dirtg_ur;
 		}
 	      if( !MAP[x-1][y] )
 		{
-		  return *dirtg_ul;
+		  return dirtg_ul;
 		}
-	      return *dirtg_u;
+	      return dirtg_u;
 	    }
 	  if( !MAP[x+1][y] )
 	    {
-	      return *dirtg_r;
+	      return dirtg_r;
 	    }
 	  if( !MAP[x-1][y] )
 	    {
-	      return *dirtg_l;
+	      return dirtg_l;
 	    }
 	}
     }
@@ -502,21 +493,21 @@ SDL_Surface kind_of_dirt(int x, int y)
 	{
 	  if( !MAP[x][y+1] ) 
 	    {
-	      return *dirtg_ur;
+	      return dirtg_ur;
 	    }
 	  if( !MAP[x][y-1] )
 	    {
-	      return *dirtg_dr;
+	      return dirtg_dr;
 	    }
-	  return *dirtg_r;
+	  return dirtg_r;
 	}
       if( !MAP[x][y+1] )
 	{
-	  return *dirtg_u;
+	  return dirtg_u;
 	}
       if( !MAP[x][y-1] )
 	{
-	  return *dirtg_d;
+	  return dirtg_d;
 	}
     }
   
@@ -527,21 +518,21 @@ SDL_Surface kind_of_dirt(int x, int y)
 	{
 	  if( !MAP[x][y+1] ) 
 	    {
-	      return *dirtg_ul;
+	      return dirtg_ul;
 	    }
 	  if( !MAP[x][y-1] )
 	    {
-	      return *dirtg_dl;
+	      return dirtg_dl;
 	    }
-	  return *dirtg_r;
+	  return dirtg_r;
 	}
       if( !MAP[x][y+1] )
 	{
-	  return *dirtg_u;
+	  return dirtg_u;
 	}
       if( !MAP[x][y-1] )
 	{
-	  return *dirtg_d;
+	  return dirtg_d;
 	}
     }
   
@@ -552,33 +543,33 @@ SDL_Surface kind_of_dirt(int x, int y)
 	{
 	  if( !MAP[x+1][y] )
 	    {
-	      return *dirtg_dr;
+	      return dirtg_dr;
 	    }
 	  if( !MAP[x-1][y] )
 	    {
-	      return *dirtg_dl;
+	      return dirtg_dl;
 	    }
-	  return *dirtg_d;
+	  return dirtg_d;
 	}
       if( !MAP[x][y-1] )
 	{
 	  if( !MAP[x+1][y] )
 	    {
-	      return *dirtg_ur;
+	      return dirtg_ur;
 	    }
 	  if( !MAP[x-1][y] )
 	    {
-	      return *dirtg_ul;
+	      return dirtg_ul;
 	    }
-	  return *dirtg_u;
+	  return dirtg_u;
 	}
       if( !MAP[x+1][y] )
 	{
-	  return *dirtg_r;
+	  return dirtg_r;
 	}
       if( !MAP[x-1][y] )
 	{
-	  return *dirtg_l;
+	  return dirtg_l;
 	}
     }
 }
