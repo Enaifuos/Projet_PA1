86ed62fef46739b2899f3a2e76d55ba2667715ed
Alex Ginestra <ginestra3u@fst-ats-312-l06.fst.site.univ-lorraine.fr>
2016-11-23 17:47:54 +0100

    creating function usefull to set the cave

--- cave_function.c
+++ cave_function.c
@@ -1,5 +1,42 @@
 #include "setting.c"
+#define CAVElength 10
+#define CAVEheight 8
 
 
+
+/*----- prototypes -----*/
 void cave();
+void set_cave(SDL_Surface * map);
+
+
+
+
+
+
+
+/*------ function -----*/
+
+void cave()
+{
+  SDL_Surface * CAVE;
+  CAVE = (SDL_Surface *)malloc(sizeof(SDL_Surface)*CAVElength*CAVEheight);
+  set_cave(CAVE);
+  
+  
+  free(CAVE);
+}
+
+
+void set_cave(SDL_Surface * map)
+{
+  int i, j;
+  for( i = 0 ; i < CAVEheight ; i++ )
+    {
+      for( j= 0 ; j < CAVElength ; j++ )
+	{
+	  map[j+i*CAVEheight] = *grass; //have to be changed by cave ground
+	}
+    }
+}
+
 
